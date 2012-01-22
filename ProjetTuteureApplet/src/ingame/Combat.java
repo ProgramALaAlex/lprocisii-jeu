796cbe2c6f42f848ce23a19d549bbad40f90ec0a
Arkanen <Arkanen@gmail.com>
2012-01-22 17:41:49 +0000

    Ajout des animations de combat pour que ce soit plus compréhensible.

--- ProjetTuteureApplet/src/ingame/Combat.java
+++ ProjetTuteureApplet/src/ingame/Combat.java
@@ -20,8 +20,12 @@ public class Combat extends BasicGameState{
 	private int stateID;
 	private ArrayList<Monstre> listeMonstre;
 
-	private boolean attaquer, selectionCible, tourJoueur;
+	private boolean attaquer, selectionCible, tourJoueur, tourEnnemi;
 	private int definirCible, tour;
+	private Combattant enCours;
+
+	private int attaqueJoueurX, destinationAttaqueJoueurX; // peut pas mettre dans constante car par rapport au bord..
+	private boolean joueurAttaque; //moche
 
 	public Combat(int stateID){
 		this.stateID = stateID;
@@ -52,22 +56,34 @@ public class Combat extends BasicGameState{
 		attaquer = true;
 		selectionCible = false;
 		tourJoueur=false;
+		tourEnnemi=false;
 		tour=0;
 		definirCible=0;
 
+		attaqueJoueurX = container.getWidth()-80;
+		destinationAttaqueJoueurX = container.getWidth()-80 - 30;
+		joueurAttaque = false;
+
+		Exploration.getPlayer().setXCombat(attaqueJoueurX);
+		for(Monstre m : listeMonstre){
+			m.setXCombat(Constantes.POSX_COMBAT_MONSTRE);
+		}
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-		Exploration.getPlayer().getSprite().draw(container.getWidth()-80,container.getHeight()/2);
+		Exploration.getPlayer().getSprite().draw(Exploration.getPlayer().getXCombat(),container.getHeight()/2);
 
 		int espace = 100;
 		for(Monstre m : listeMonstre){
-			m.getSprite().draw(100, (espace+=50));
+			m.getSprite().draw(m.getXCombat(), (espace+=60));
+			g.setColor(Color.black);
+			g.drawString(m.getPvCourant()+"/"+m.getPvMax(), m.getXCombat(), espace+46);
+			g.setColor(Color.gray);
+			g.drawString(m.getPvCourant()+"/"+m.getPvMax(), m.getXCombat(), espace+45);
 		}
 
 		// Menu combat
-		g.setColor(Color.gray);
 		int positionH = container.getHeight()-70;
 		g.drawLine(0, positionH, container.getWidth(), positionH);
 		if (attaquer){
@@ -85,7 +101,7 @@ public class Combat extends BasicGameState{
 			g.drawString(Constantes.SELECTION+"Utiliser potion", 50, positionH+30);
 			g.setColor(Color.gray);
 		}
-		g.drawString("PV : "+Exploration.getPlayer().getPvCourant()+"/"+Exploration.getPlayer().getPvMax(), 300, positionH+10);
+		g.drawString(Exploration.getPlayer().getPvCourant()+"/"+Exploration.getPlayer().getPvMax(), Exploration.getPlayer().getXCombat()-20, container.getHeight()/2+35);
 	}
 
 	@Override
@@ -101,26 +117,34 @@ public class Combat extends BasicGameState{
 		listeCombattant.add(Exploration.getPlayer());
 		listeCombattant.addAll(this.listeMonstre);
 
-		if(!tourJoueur){
+		if(!tourJoueur && !tourEnnemi){
 			if(tour>=listeCombattant.size())
 				tour=0;
-			Combattant c = getPlusRapide(listeCombattant, tour);
-			if(c instanceof Player){
+			enCours = getPlusRapide(listeCombattant, tour);
+			if(enCours instanceof Player){
 				tourJoueur=true;
-				choisirAction(input);
+				tourEnnemi=false;
+				choisirAction(input, delta);
 			}
 			else{
-				c.attaquer(Exploration.getPlayer());
+				tourEnnemi=true;
+				enCours.attaquer(Exploration.getPlayer());
 			}
-			
+
 			//fin du tour
 			tour++;
 		}
-		if(tourJoueur){
-			choisirAction(input);
+		// c'est au tour de l'ennemi d'attaquer
+		if(tourEnnemi){
+			if(enCours.deplacementAttaque(delta, Constantes.POSX_COMBAT_MONSTRE, Constantes.POSX_ATTAQUE_MONSTRE))
+				tourEnnemi = false;
 		}
-		
-		
+
+		if(tourJoueur){
+			choisirAction(input, delta);
+		}
+
+
 		//si tous les ennemis sont mort, on arrête le combat
 		if(listeMonstre.isEmpty()){
 			Exploration.getPlayer().finCombat();
@@ -153,7 +177,7 @@ public class Combat extends BasicGameState{
 		ArrayList<Combattant> temp = (ArrayList<Combattant>) listeCombattant.clone();
 		ArrayList<Combattant> sortedList = new ArrayList<Combattant>();
 		Combattant rapide = null;
-		
+
 		for (int i=0; i<listeCombattant.size(); i++){
 			int plusRapide=0;
 			for (Combattant c : temp){
@@ -174,38 +198,46 @@ public class Combat extends BasicGameState{
 	 * Autorise le joueur à choisir une action dans le menu
 	 * (c'est donc à son tour de jouer)
 	 */
-	public void choisirAction(Input input){
+	public void choisirAction(Input input, int delta){
 		// naviguation dans le "menu"
-		if(!selectionCible){
-			if(input.isKeyPressed(Input.KEY_DOWN) || input.isKeyPressed(Input.KEY_UP))
-				attaquer = !attaquer;
 
-			if(input.isKeyPressed(Input.KEY_ENTER)){
-				// si pas encore en train de choisir cible, choisir attaquer (menu bas)
-				if(attaquer && !selectionCible){
-					selectionCible = true;
+		if(!joueurAttaque){
+			if(!selectionCible){
+				if(input.isKeyPressed(Input.KEY_DOWN) || input.isKeyPressed(Input.KEY_UP))
+					attaquer = !attaquer;
+
+				if(input.isKeyPressed(Input.KEY_ENTER)){
+					// si pas encore en train de choisir cible, choisir attaquer (menu bas)
+					if(attaquer && !selectionCible){
+						selectionCible = true;
+					}
+					else {
+						Exploration.getPlayer().utiliserPotion();
+						tourJoueur=false;
+					}
 				}
-				else {
-					System.out.println("UTILISER POTION");
-					Exploration.getPlayer().utiliserPotion();
-					tour++;
-					tourJoueur=false;
+			}
+
+			//selection d'une cible
+			else {
+				if(input.isKeyPressed(Input.KEY_DOWN))
+					if(definirCible < listeMonstre.size()-1)
+						definirCible++;
+					else definirCible=0;
+				else if(input.isKeyPressed(Input.KEY_UP))
+					if(definirCible>0)
+						definirCible--;
+					else definirCible=listeMonstre.size()-1;
+				else if(input.isKeyPressed(Input.KEY_ENTER)){
+					// on attaque
+					joueurAttaque = true;
+
 				}
 			}
 		}
-
-		//selection d'une cible
 		else {
-			if(input.isKeyPressed(Input.KEY_DOWN))
-				if(definirCible < listeMonstre.size()-1)
-					definirCible++;
-				else definirCible=0;
-			else if(input.isKeyPressed(Input.KEY_UP))
-				if(definirCible>0)
-					definirCible--;
-				else definirCible=listeMonstre.size()-1;
-			else if(input.isKeyPressed(Input.KEY_ENTER)){
-				// on attaque
+			if (Exploration.getPlayer().deplacementAttaque(delta, attaqueJoueurX, destinationAttaqueJoueurX)){
+				joueurAttaque = false;
 				if(Exploration.getPlayer().attaquer(listeMonstre.get(definirCible))){
 					// si l'ennemi est KO
 					Exploration.getPlayer().getInventaire().addObjets(listeMonstre.get(definirCible).drop());
