15a29fb418c28430cb9a9672bc38398011a40182
yannick.schurter <yannick.schurter@gmail.com>
2012-03-19 18:47:04 +0000

    tags cliquables

---
+++ UnJeu/src/java/command/ConnectCommand.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package command;
+
+import beans.JoueurBean;
+import beans.JoueurDB;
+import javax.servlet.http.*;
+
+/**
+ *
+ * @author Yan
+ */
+public class ConnectCommand implements Command {
+
+    @Override
+    public String getCommandName() {
+        return "connectCommand";
+    }
+
+    @Override
+    public ActionFlow actionPerform(HttpServletRequest request) {
+        String vue = "index";
+
+        HttpSession session = request.getSession(false);
+
+        String login = request.getParameter("login");
+        String password = request.getParameter("pass");
+        
+        if (!login.equals("") && !password.equals("")) {
+            JoueurDB db = new JoueurDB();
+            JoueurBean joueur;
+            if ((joueur = db.checkJoueur(login, password)) != null) {
+                System.out.println("Connection successful");
+                session.setAttribute("id", joueur.getIdJoueur());
+                session.setAttribute("pseudo", joueur.getPseudo());
+            } else {
+                System.out.println("connection failed");
+            }
+        }
+        return new ActionFlow(vue, vue + ".jsp", false);
+    }
+
+}
---
+++ UnJeu/src/java/command/SubscribeCommand.java
@@ -0,0 +1,59 @@
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package command;
+
+import beans.JoueurDB;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.*;
+
+/**
+ * @author Yan
+ */
+public class SubscribeCommand implements Command {
+
+    @Override
+    public String getCommandName() {
+        return "subscribeCommand";
+    }
+
+    @Override
+    public ActionFlow actionPerform(HttpServletRequest request) {
+        String vue = "index";
+        String erreur = "erreur";
+        
+        System.out.println(this.getCommandName());
+        
+        
+        String pseudo = request.getParameter("pseudo");
+        String mail = request.getParameter("mail");
+        String pass1 = request.getParameter("pass1");
+        String pass2 = request.getParameter("pass2");
+        
+        JoueurDB db = new JoueurDB();
+        
+        // si les champs ne sont pas remplis
+        if(pseudo.equals("") || mail.equals("") || pass1.equals("") || pass1.equals(""))
+            request.setAttribute("erreur", "Erreur : tous les champs ne sont pas remplis.");
+        else
+        
+        // Si un utilisateur existe déjà
+        if (db.existe(pseudo)) {
+            // Plus tard :)
+        }
+        // Si tout est OK
+        else if(pass1.equals(pass2)){
+            
+        }
+        else {
+            request.setAttribute("erreur", "Erreur : mot de passe.");
+        }
+        
+        return new ActionFlow(vue, vue + ".jsp", false);
+    }
+
+}
